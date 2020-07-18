2043cca7ca517192ab8376e7d555b29c6143a562
rijan-chapagain <hrijon.chp@gmail.com>
2020-07-18 15:13:25 +0800

    render moves

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { calculateWinner } from '../helpers';
 import Board from './Board';
 
+const styles = {
+    width: '200px',
+    margin: '20px auto'
+}
+
 const Game = () => {
 
     const [board, setBoard] = useState(Array(9).fill(null));
@@ -23,14 +28,28 @@ const Game = () => {
         
     }
     
-    const renderMoves = () => {
-        
-    }
+    const renderMoves = () => (
+        <button onClick={() => setBoard(Array(9).fill(null))}>
+            Start Game
+        </button>
+    )
+
+    // {
+    //     return <button onClick={() => setBoard(Array(9).fill(null))}></button>
+    // }
 
 
     return (
-        <Board squares={board} onClick={handleClick} />
-    )
+        // it can only return one tag co wrap in fragment
+        // fragment is empty tag 
+        <>
+            <Board squares={board} onClick={handleClick} />
+            <div style={styles}>
+                <p>{winner ? 'Winner: ' + winner: 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+                {renderMoves()}
+            </div>
+        </>
+    );
 }
 
 export default Game;
