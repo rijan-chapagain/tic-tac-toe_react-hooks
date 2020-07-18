f648dcec0998cc708f7d65eb19b73abf94a91aa7
rijan-chapagain <hrijon.chp@gmail.com>
2020-07-18 15:29:44 +0800

    time travel implementation

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,31 +8,46 @@ const styles = {
 }
 
 const Game = () => {
-
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
     const [board, setBoard] = useState(Array(9).fill(null));
     const [xIsNext, setXisNext] = useState(true);
-    const winner = calculateWinner(board);
+    const winner = calculateWinner(history[stepNumber]);
 
     const handleClick = i => {
-        // always make copy for mutation
-        const boardCopy = [...board];
-        // if user click an occupied square or if game is won, return 
-        if(winner || boardCopy[i]) return;
-        // Put an X or O in the clicked square
-        boardCopy[i] = xIsNext ? 'X' : 'O';
-        setBoard(boardCopy);
-        setXisNext(!xIsNext);
-    }
 
-    const jumpTo = () => {
-        
-    }
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+
+        // always make copy for mutation
+        // const boardCopy = [...board];
+
+        // if user click an occupied square or if game is won, return 
+        if(winner || squares[i]) return;
+        // Put an X or O in the clicked square
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length)
+
+        setXisNext(!xIsNext);
+    };
+
+    const jumpTo = step => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0)
+    };
     
     const renderMoves = () => (
-        <button onClick={() => setBoard(Array(9).fill(null))}>
-            Start Game
-        </button>
-    )
+        history.map((_step, move) => {
+            const destination = move ? `Go to move#${move}` : ' Go to start';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            );
+        })
+    );
 
     // {
     //     return <button onClick={() => setBoard(Array(9).fill(null))}></button>
@@ -43,7 +58,7 @@ const Game = () => {
         // it can only return one tag co wrap in fragment
         // fragment is empty tag 
         <>
-            <Board squares={board} onClick={handleClick} />
+            <Board squares={history[stepNumber]} onClick={handleClick} />
             <div style={styles}>
                 <p>{winner ? 'Winner: ' + winner: 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
                 {renderMoves()}
